92db0307204b2de5c4ed988369e6a068460a6817
Sen <keima.ayasaki@gmail.com>
2018-05-08 19:13:54 -0600

    Added a test scene under which tapping is interpreted correctly (needs to be tested on an actual device)

---
+++ GameClient/IdealJourneyClient/Assets/Scripts/Sen/CubeTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTest : MonoBehaviour {
+
+    [Header("Configuration")]
+    [Header("Internal Information")]
+    [SerializeField] [Range(1.0f, 20.0f)] private float m_Speed = 20.0f;
+
+    private Vector3 m_RotationDirection = Vector3.up;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(m_RotationDirection * Time.deltaTime * m_Speed);
+	}
+
+    public void ToggleRotationDirection()
+    {
+        Debug.Log("Toggling rotation direction");
+
+        if (m_RotationDirection == Vector3.up)
+        {
+            m_RotationDirection = Vector3.down;
+        }
+        else
+        {
+            m_RotationDirection = Vector3.up;
+        }
+    }
+
+}
