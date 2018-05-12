b69e83da5663b623e46110c09212563ed2b8812d
mvanderlip <mvanderlip@student.neumont.edu>
2018-05-11 18:10:42 -0600

    test: Added gyroscope/accelerometer tests

---
+++ IdealJourneyClient/Assets/TestingArea/TestScripts/TestAccel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestAccel : MonoBehaviour {
+
+    private float speed = 1;
+    private Text m_text;
+
+    private void Start()
+    {
+        m_text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 dir = Vector3.zero;
+        dir.x = -Input.acceleration.y;
+        dir.z = Input.acceleration.x;
+        AccelerationEvent[] events = Input.accelerationEvents;
+
+        foreach (var item in events)
+        {
+            if (item.acceleration.ToString() != Vector3.back.ToString())
+            {
+                Debug.Log("Start");
+                m_text.text = item.acceleration.ToString();
+
+                Debug.Log("End");
+            }
+        }
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+        //Debug.Log(dir.x);
+        //Debug.Log(dir.z);
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
+
+    }
+}
---
+++ IdealJourneyClient/Assets/TestingArea/TestScripts/TestGyro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGyro : MonoBehaviour {
+
+    private Gyroscope m_gyro;
+
+	// Use this for initialization
+	void Start () {
+        m_gyro = Input.gyro;
+        m_gyro.enabled = true;
+        Screen.autorotateToLandscapeLeft = false;
+        Screen.autorotateToLandscapeRight = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.autorotateToPortrait = true;
+        Screen.orientation = ScreenOrientation.AutoRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_gyro = Input.gyro;
+        transform.rotation = m_gyro.attitude;
+
+        transform.rotation = Quaternion.Euler(-transform.rotation.eulerAngles.x, -transform.rotation.eulerAngles.z, -transform.rotation.eulerAngles.y);
+	}
+}
