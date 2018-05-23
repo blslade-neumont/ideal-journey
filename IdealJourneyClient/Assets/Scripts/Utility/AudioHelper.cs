a345b34cfd04a9725a50e32112e1060ba60b0250
Justin Furtado <justin.l.furtado@gmail.com>
2018-05-22 18:22:52 -0600

    Null-check fixes bugs

--- IdealJourneyClient/Assets/Scripts/Utility/AudioHelper.cs
+++ IdealJourneyClient/Assets/Scripts/Utility/AudioHelper.cs
@@ -3,41 +3,50 @@
 public static class AudioHelper
 {
     public static float BGMMultiplier = 1.0f;
-    public static float SFXMultiplier = 1.0f;
     public static float VoiceMultiplier = 1.0f;
+    public static float SFXMultiplier = 1.0f;
     public static bool BGMEnabled = true;
     public static bool SFXEnabled = true;
     public static bool VoiceEnabled = true;
 
     public static void InitSFX(AudioSource sfx)
     {
-        sfx.volume *= SFXMultiplier;
-        if (sfx.isPlaying && !SFXEnabled) { sfx.Stop(); }
+        if (sfx != null)
+        {
+            sfx.volume *= SFXMultiplier;
+            if (sfx.isPlaying && !SFXEnabled) { sfx.Stop(); }
+        }
     }
 
     public static void PlaySFX(AudioSource sfx)
     {
-        if (SFXEnabled) { sfx.Play(); }
+        if (SFXEnabled && sfx != null) { sfx.Play(); }
     }
     public static void InitBGM(AudioSource bgm)
     {
-        bgm.volume *= BGMMultiplier;
-        if (bgm.isPlaying && !BGMEnabled) { bgm.Stop(); }
+        if (bgm != null)
+        {
+            bgm.volume *= BGMMultiplier;
+            if (bgm.isPlaying && !BGMEnabled) { bgm.Stop(); }
+        }
     }
 
     public static void PlayBGM(AudioSource bgm)
     {
-        if (BGMEnabled) { bgm.Play(); }
+        if (BGMEnabled && bgm != null) { bgm.Play(); }
     }
 
     public static void InitVoice(AudioSource voice)
     {
-        voice.volume *= BGMMultiplier;
-        if (voice.isPlaying && !VoiceEnabled) { voice.Stop(); }
+        if (voice != null)
+        {
+            voice.volume *= VoiceMultiplier;
+            if (voice.isPlaying && !VoiceEnabled) { voice.Stop(); }
+        }
     }
 
     public static void PlayVoice(AudioSource voice)
     {
-        if (VoiceEnabled) { voice.Play(); }
+        if (VoiceEnabled && voice != null) { voice.Play(); }
     }
 }
