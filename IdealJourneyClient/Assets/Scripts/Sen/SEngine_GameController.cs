784707b2ca35ec8bf1febc96e4bd7f403247ef51
Sen <keima.ayasaki@gmail.com>
2018-05-23 19:09:18 -0600

    Added an action log that can be modified to affect weights within a test scene. See SEn's gine GameController and the corresponding Sen_GameScene.

--- IdealJourneyClient/Assets/Scripts/Sen/SEngine_GameController.cs
+++ IdealJourneyClient/Assets/Scripts/Sen/SEngine_GameController.cs
@@ -1,159 +1,230 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(SceneMover))]
-public class SEngine_GameController : MonoBehaviour {
-
+[RequireComponent(typeof(SceneMover), typeof(AudioSource))]
+public class SEngine_GameController : MonoBehaviour
+{
     [Header("Configuration")]
     [Header("Internal Information")]
     [Header("Turn Duration Information")]
-    [SerializeField] [Range(0.0f, 10.0f)] private float m_currentTimeToCompleteAction   = 10.0f;
     [SerializeField] [Range(1, 100)] private int m_decay = 5;
-    [SerializeField] [Range(0.0f, 10.0f)] private float m_minimumTimeToCompleteAction   = 5.0f;
-    [SerializeField] [Range(0.0f, 10.0f)] private float m_maximumTimeToCompleteAction   = 10.0f;
+    [SerializeField] [Range(0.0f, 10.0f)] private float m_minimumTimeToCompleteAction = 5.0f;
+    [SerializeField] [Range(0.0f, 10.0f)] private float m_maximumTimeToCompleteAction = 10.0f;
 
     [Header("Turn Delay Information")]
-    [SerializeField] [Range(0.0f, 10.0f)] private float m_currentTimeTilNextTurn        = 2.0f;
-    [SerializeField] [Range(0.0f, 10.0f)] private float m_minimumTimeTilNextTurn        = 0.5f;
-    [SerializeField] [Range(0.0f, 10.0f)] private float m_maximumTimeTilNextTurn        = 2.0f;
-    
-    [Header("Debug Mode")]
-    [SerializeField] private bool m_debugMode = false;
+    [SerializeField] [Range(0.0f, 10.0f)] private float m_minimumTimeTilNextTurn = 0.5f;
+    [SerializeField] [Range(0.0f, 10.0f)] private float m_maximumTimeTilNextTurn = 2.0f;
+    [SerializeField] [Range(0.0f, 10.0f)] private float m_startBreakTime = 3.0f;
 
-    // Private internal information
-    private float       m_timeElapsed = 0.0f;
-    private int         m_turnCount = 0;
-    private int         m_currentState = -1;
-    private String[]    m_commands;
-    private bool        m_waitingForNextTurn = false;
+    [Header("Debug - Exposed Variables")]
+    [Tooltip("0: Tap\n1: Shake\n2: Flip\n3: Turn\n4: Swipe")]
+    [SerializeField] private int m_actionLogSize;
+    [SerializeField] private int[] m_actionLog;
+    [SerializeField] private float[] m_actionWeight;
 
-    // Private sibling components
+    private GameCommand[] m_commands;
+    private MyTimer m_timer;
+    private int m_currentCommandIndex = -1;
+    private System.Random m_randGen = new System.Random();
+
     private SceneMover m_sceneMover;
+    private AudioSource m_successSFX;
 
-	// Use this for initialization
-	void Awake () {
-        if (m_debugMode)
+    public bool IsWaitingForAction { get; private set; }
+    public int CompletedActions { get; private set; }
+
+    public GameCommand CurrentCommand
+    {
+        get { return m_commands[m_currentCommandIndex]; }
+    }
+
+    public float TimeLeft
+    {
+        get { return m_timer.TimeRemaining; }
+    }
+
+    private void Awake()
+    {
+        GetSiblingComponents();
+        InitializeCommands();
+        InitializeTimer();
+    }
+
+    private void InitializeCommands()
+    {
+        m_commands = new GameCommand[]
         {
-            print("Game Start");
-        }
-        // Get sibling components
-        m_sceneMover = GetComponent<SceneMover>();
-
-        m_commands = new String[]{
-            "TAP",
+            new TapCommand(),
+            new ShakeCommand(),
+            new FlipCommand(),
+            new TurnCommand(),
+            new SwipeCommand()
         };
 
-        GetRandomState();
-
-        if(m_debugMode)
+        m_actionLog = new int[m_actionLogSize];
+        for(int x = 0; x < m_actionLogSize; ++x)
         {
-            print(m_commands[m_currentState] + " IT! Time to complete: [" + m_currentTimeToCompleteAction.ToString() + "].");
+            m_actionLog[x] = -1;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-        // Iterate timer
-        m_timeElapsed += Time.deltaTime;
 
-        // Decide what the timer is doing
-        if (m_waitingForNextTurn)
-        {
-            // The timer exceeded the delay for the next turn
-            if(m_timeElapsed > m_currentTimeTilNextTurn)
-            {
-                SetCompletionTime();
-                GetRandomState();
-                m_timeElapsed = 0.0f;
-                m_waitingForNextTurn = false;
-
-                if(m_debugMode)
-                {
-                    print(m_commands[m_currentState] + " IT! Time to complete: [" + m_currentTimeToCompleteAction + "].");
-                }
-            }
-            return;
-        }
-        else
-        {
-            // Should the complete the function, add to score, reset timer, and destroy current state
-            if (ActionIsComplete())
-            {
-                // Reset Timer
-                m_timeElapsed = 0.0f;
-                // Iterate score
-                ++m_turnCount;
-                // start delay
-                m_waitingForNextTurn = true;
-                // Set turn delay
-                SetTurnDelay();
-            }
-            // Action was not completed
-            else if (m_timeElapsed > m_currentTimeToCompleteAction)
-            {
-                EndGame();
-            }
-        }
-    }
-    private static float QuadraticOut(float t)
+    private void AddToActionLog(int actionIndex)
     {
-        return -(t * (t - 2.0f));
-    }
-
-    private void SetCompletionTime()
-    {
-        float t = Mathf.Clamp01(m_turnCount / m_decay);
-        m_currentTimeToCompleteAction = Mathf.Lerp(m_maximumTimeToCompleteAction, m_minimumTimeToCompleteAction, QuadraticOut(t));
-    }
-
-
-    private void SetTurnDelay()
-    {
-        float timeLeft = m_currentTimeToCompleteAction - m_timeElapsed;
-        m_currentTimeTilNextTurn = Mathf.Clamp(timeLeft, m_minimumTimeTilNextTurn, m_maximumTimeTilNextTurn);
-
-        if (m_debugMode)
+        for(int x = m_actionLogSize - 2; x > -1; --x)
         {
-            print("Action took [" + m_timeElapsed.ToString() + "] to complete.");
-            print("Action Complete; Waiting out timer [" + m_currentTimeTilNextTurn.ToString() +" seconds].");
+            m_actionLog[x + 1] = m_actionLog[x];
+        }
+        m_actionLog[0] = actionIndex;
+        ExamineActionLogForRepitition();
+    }
+
+    private void ExamineActionLogForRepitition()
+    {
+        // Stores the frequencies of numbers
+        int[] frequencies = new int[m_commands.Length];
+        
+        for(int x = 0; x < m_actionLogSize; ++x)
+        {
+            int commandIndex = m_actionLog[x];
+
+            // Ignores invalid locations from initial set up
+            if(commandIndex == -1)
+            {
+                continue;
+            }
+
+            // Iterate frequency
+            ++frequencies[commandIndex];
+        }
+
+        // Calculate inverses
+        int max = frequencies.Sum();
+
+        // Calculate percentages
+        // Create frequency board (cached)
+        // Pick from frequency board
+    }
+    private void GetSiblingComponents()
+    {
+        m_successSFX = GetComponent<AudioSource>();
+        m_sceneMover = GetComponent<SceneMover>();
+    }
+
+    private void InitializeTimer()
+    {
+        m_timer = new MyTimer(m_startBreakTime, OnBreakOver); // start with a break :)
+    }
+
+    private void OnOutOfActionTime()
+    {
+        EndGame();
+    }
+
+    private void OnBreakOver()
+    {
+        BeginAction();
+    }
+
+    private void BeginAction()
+    {
+        SetupActionState();
+        SetupActionTimer();
+        NotifyActionToDoToUser();
+    }
+
+    private void NotifyActionToDoToUser()
+    {
+        CurrentCommand.PlayVoiceOverSFX();
+    }
+
+    private void SetupActionState()
+    {
+        PickNextCommand();
+        InitializeNewCommand();
+        IsWaitingForAction = true;
+    }
+
+    private void InitializeNewCommand()
+    {
+        CurrentCommand.Begin();
+    }
+
+    private void BeginBreak()
+    {
+        SetupBreakState();
+        SetupBreakTimer();
+    }
+
+    private void SetupBreakState()
+    {
+        IsWaitingForAction = false;
+    }
+
+    private void SetupActionTimer()
+    {
+        m_timer.CycleTime = CalculateCompletionTime();
+        m_timer.OnCycle = OnOutOfActionTime;
+    }
+
+    private void SetupBreakTimer()
+    {
+        m_timer.CycleTime = CalculateTimeUntilNextTurn();
+        m_timer.OnCycle = OnBreakOver;
+    }
+
+    private void Update()
+    {
+        m_timer.TickTimer(Time.deltaTime);
+        if (IsWaitingForAction && ActionIsComplete())
+        {
+            OnActionCompletedSuccessfully();
         }
     }
 
-    /// <summary>
-    /// Processes the action required for someone to carry out
-    /// </summary>
-    /// <returns>A boolean describing the completion of a requirement</returns>
+    private void OnActionCompletedSuccessfully()
+    {
+        ++CompletedActions;
+        NotifySuccessToUser();
+        BeginBreak();
+    }
+
+    private void NotifySuccessToUser()
+    {
+        AudioHelper.PlaySFX(m_successSFX);
+        VibrateHelper.Vibrate();
+    }
+
+    private float CalculateCompletionTime()
+    {
+        float t = Mathf.Clamp01(CompletedActions / (float)m_decay);
+        return Mathf.Lerp(m_maximumTimeToCompleteAction, m_minimumTimeToCompleteAction, QuadraticOut(t));
+    }
+
+    private float CalculateTimeUntilNextTurn()
+    {
+        return Mathf.Clamp(m_timer.TimeRemaining, m_minimumTimeTilNextTurn, m_maximumTimeTilNextTurn);
+    }
+
     private bool ActionIsComplete()
     {
-        // needs implementation
-
-        // If action succeeded, return true
-        if (Input.GetKey(KeyCode.Space))
-        {
-            return true;
-        }
-        return false;
+        return CurrentCommand.IsComplete();
     }
 
-    bool first = true;
-    private void GetRandomState()
+    private void PickNextCommand()
     {
-        m_currentState = 0;
-
-        if (m_debugMode)
-        {
-            print("Next task: [" + m_commands[m_currentState] + "].");
-        } 
+        //AddToActionLog();
     }
 
     private void EndGame()
     {
-        if(m_debugMode)
-        {
-            print("Game Ending");
-        }
         m_sceneMover.MoveToEnd();
+    }
+
+    private static float QuadraticOut(float t)
+    {
+        return -(t * (t - 2.0f));
     }
 }
