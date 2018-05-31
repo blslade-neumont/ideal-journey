f960d797d978abac1efa33901567c3afcc458863
mvanderlip <mvanderlip@student.neumont.edu>
2018-05-30 18:35:23 -0600

    Removed error in LoginScene.unity, username to password error.

---
+++ IdealJourneyClient/Assets/Scripts/Utility/AutoToPassword.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AutoToPassword : MonoBehaviour {
+
+    private EventSystem m_eventSystem;
+    [SerializeField] private GameObject m_password;
+
+    private void Start()
+    {
+        m_eventSystem = EventSystem.current;
+    }
+
+    public void ToPassword()
+    {
+        if (!m_eventSystem.alreadySelecting)
+        {
+            m_eventSystem.SetSelectedGameObject(m_password);
+        }
+    }
+}
