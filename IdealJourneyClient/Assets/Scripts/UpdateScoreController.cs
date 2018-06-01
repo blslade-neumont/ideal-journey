64a0ff76f523ee9116f4035a7d4829f13810de4c
Justin Furtado <justin.l.furtado@gmail.com>
2018-05-31 18:46:19 -0600

    Fix bug and alter difficulty curve

--- IdealJourneyClient/Assets/Scripts/UpdateScoreController.cs
+++ IdealJourneyClient/Assets/Scripts/UpdateScoreController.cs
@@ -19,14 +19,16 @@ public class UpdateScoreController : MonoBehaviour
     private void Awake()
     {
         var score = GameController.LastScore;
-        var bestScore = AuthController.CurrentAuthToken.user.bestScore;
         if (m_txtScore != null) m_txtScore.text = "" + score;
 
         m_txtNewHighScore = GetComponent<Text>();
 
-        if (AuthController.CurrentAuthToken == null || AuthController.CurrentAuthToken.user == null || score <= bestScore)
+        if (AuthController.CurrentAuthToken == null || AuthController.CurrentAuthToken.user == null || score <= AuthController.CurrentAuthToken.user.bestScore)
         {
-            m_txtNewHighScore.text = "Your best:\n" + bestScore;
+            if (AuthController.CurrentAuthToken == null || AuthController.CurrentAuthToken.user == null) { m_txtNewHighScore.enabled = false; }
+            else { m_txtNewHighScore.text = "Your best:\n" + AuthController.CurrentAuthToken.user.bestScore; }
+        
+
             if (m_txtSendStatus != null) m_txtSendStatus.enabled = false;
             if (m_loadingSpinner != null) m_loadingSpinner.SetActive(false);
             return;
